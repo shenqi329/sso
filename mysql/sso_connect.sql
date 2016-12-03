b0040362eea4965ad8e6d05260516aa23c230fc3
liujunshi <524607630@qq.com>
2016-12-03 17:59:02 +0800

    sql文件命名规范化

--- mysql/sso_connect.sql
+++
@@ -1,9 +0,0 @@
-use db_sso;
-
-select * from t_user;
-select * from t_token;
-
-INSERT INTO t_token(t_token_user_id,t_token_token) VALUES (2,"xxxxxxxx");
-
-
-INSERT INTO t_user(t_user_username,t_user_password) VALUES ("loifsd","xxxxxxxx");
--- mysql/sso_select.sql
+++
@@ -1,6 +0,0 @@
-use db_sso;
-
-select * from t_user;
-select * from t_token;
-
-select * from t_token where token_user_id = 1;
